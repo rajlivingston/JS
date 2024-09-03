7cde980edf618fe34219a1f19cadb2c4eb735a62
Livingston <liwingston2003@gmail.com>
2024-09-03 09:55:57 +0530

    Practising JS

---
+++ Day-14/nestedObjMerge.js
@@ -0,0 +1,11 @@
+const data = {
+    info: { name: 'Alice', age: 25 },
+    address: { city: 'Wonderland', zip: '12345' }
+  };
+  const key = 'address';
+  const updated = {
+    ...data,
+    [key]: { ...data[key], country: 'Fictionland' }
+  };
+  console.log(updated); 
+  
